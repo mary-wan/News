14a5e322f2d89c1615b75d1738d1f7a821224965
mary-wan <mary.njenga@student.moringaschool.com>
2022-03-01 15:58:40 +0300

    refactor

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     '''
     NEWS_API_SOURCES_URL = 'https://newsapi.org/v2/sources?apiKey={}'
-    NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/sources?language=en&country=us&apiKey={}'
+    NEWS_API_ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     NEWS_API_KEY = 'aa251654e1b04b0d9917f813d99cb1a9'
 
 class DevConfig(Config):
